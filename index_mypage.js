4765ea6acbb89ccfd6e37492c64ba4f08636bcfa
Diana Fernández <129325522+Ditartemisa@users.noreply.github.com>
2023-08-11 11:33:52 -0600

    initial commit

---
+++ index_mypage.js
@@ -0,0 +1,45 @@
+//creo función de ejecución(Click boton)
+function muestra_oculta(contenido)
+{
+//declaro función y paso parámetro selector de referencia
+let div = document.getElementById(contenido);
+//uso condicional id para saber si el div está oculto o visible
+if (div.style.display == "none")
+    {
+        div.style.display = "flex";
+    }
+else
+    {
+        div.style.display = "none";
+    }
+}
+
+function muestra_oculta(contenido2)
+{
+//declaro función y paso parámetro selector de referencia
+let div = document.getElementById(contenido2);
+//uso condicional id para saber si el div está oculto o visible
+if (div.style.display == "none")
+    {
+        div.style.display = "flex";
+    }
+else
+    {
+        div.style.display = "none";
+    }
+}
+
+function muestra_oculta(contenido3)
+{
+//declaro función y paso parámetro selector de referencia
+let div = document.getElementById(contenido3);
+//uso condicional id para saber si el div está oculto o visible
+if (div.style.display == "none")
+    {
+        div.style.display = "flex";
+    }
+else
+    {
+        div.style.display = "none";
+    }
+}
